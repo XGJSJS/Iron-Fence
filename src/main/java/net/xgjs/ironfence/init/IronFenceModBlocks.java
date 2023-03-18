5c1d98d7a3189ccefde7145b70e19a769591e5e5
XGJSJS <90537067+XGJSJS@users.noreply.github.com>
2023-03-18 21:27:41 +0800

    Fix error file

--- src/main/java/net/xgjs/ironfence/init/IronFenceModBlocks.java
+++ src/main/java/net/xgjs/ironfence/init/IronFenceModBlocks.java
@@ -1,42 +1,35 @@
 
 package net.xgjs.ironfence.init;
 
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.block.Block;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
-import net.minecraft.item.ItemStack;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.*;
+
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Identifier;
 import net.xgjs.ironfence.IronFenceMod;
 
-public class IronFenceModItems {
-	public static final Item IRON_FENCE = blockItemi(IronFenceModBlocks.IRON_FENCE);
-	public static final Item IRON_FENCE_GATE = blockItemi(IronFenceModBlocks.IRON_FENCE_GATE);
-	public static final Item NETHERITE_FENCE = blockItemi(IronFenceModBlocks.NETHERITE_FENCE);
-	public static final Item NETHERITE_FENCE_GATE = blockItemi(IronFenceModBlocks.NETHERITE_FENCE_GATE);
+import net.xgjs.ironfence.block.*;
+
+public class IronFenceModBlocks {
+	public static final WoodType IRON = new WoodType("iron", BlockSetType.IRON, BlockSoundGroup.METAL, BlockSoundGroup.METAL, SoundEvents.BLOCK_FENCE_GATE_CLOSE, SoundEvents.BLOCK_FENCE_GATE_OPEN);
+	public static final WoodType NETHERITE = new WoodType("netherite", BlockSetType.IRON, BlockSoundGroup.METAL, BlockSoundGroup.METAL, SoundEvents.BLOCK_FENCE_GATE_CLOSE, SoundEvents.BLOCK_FENCE_GATE_OPEN);
+	public static final Block IRON_FENCE = new IronFence(blockis(Blocks.IRON_BLOCK));
+	public static final Block IRON_FENCE_GATE = new IronFenceGate(blockis(Blocks.IRON_BLOCK), IRON);
+	public static final Block NETHERITE_FENCE = new NetheriteFence(blockis(Blocks.NETHERITE_BLOCK));
+	public static final Block NETHERITE_FENCE_GATE = new NetheriteFenceGate(blockis(Blocks.NETHERITE_BLOCK), NETHERITE);
+
 	public static void load() {
-		Registry.register(Registries.ITEM, id("iron_fence"), IRON_FENCE);
-		Registry.register(Registries.ITEM, id("iron_fence_gate"), IRON_FENCE_GATE);
-		Registry.register(Registries.ITEM, id("netherite_fence"), NETHERITE_FENCE);
-		Registry.register(Registries.ITEM, id("netherite_fence_gate"), NETHERITE_FENCE_GATE);
-		ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(content -> {
-			content.add(itemi(IRON_FENCE));
-			content.add(itemi(IRON_FENCE_GATE));
-			content.add(itemi(NETHERITE_FENCE));
-			content.add(itemi(NETHERITE_FENCE_GATE));
-		});
+		Registry.register(Registries.BLOCK, id("iron_fence"), IRON_FENCE);
+		Registry.register(Registries.BLOCK, id("iron_fence_gate"), IRON_FENCE_GATE);
+		Registry.register(Registries.BLOCK, id("netherite_fence"), NETHERITE_FENCE);
+		Registry.register(Registries.BLOCK, id("netherite_fence_gate"), NETHERITE_FENCE_GATE);
 	}
 
-	public static Item blockItemi(Block block) {
-		return new BlockItem(block, new FabricItemSettings());
-	}
-
-	public static ItemStack itemi(Item item) {
-		return new ItemStack(item);
+	public static AbstractBlock.Settings blockis(Block block) {
+		return FabricBlockSettings.copy(block);
 	}
 
 	public static Identifier id(String id) {
